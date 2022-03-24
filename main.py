4b46fe4f2d4279ad05006b3b8e5ffa94991d8cbd
changeAJay <ajajay008@gmail.com>
2022-03-24 20:58:38 +0530

    picked up prev closing price using API

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
+import requests
+
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
+parameters = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "interval": "5min",
+    "apikey": "P0HM4GKCWWF3ECIG"
+}
+
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
+response = requests.get(STOCK_ENDPOINT, params=parameters)
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
 
-#TODO 2. - Get the day before yesterday's closing stock price
+#Get yesterday's closing stock price. Hint: Yo can perform list comprehensions on Python dictionaries.
+# e.g. [new_value for (key, value) in dictionary.items()]
+yesterday_data = data_list[0]
+yesterday_closingprice = float(yesterday_data["4. close"])
+print(yesterday_closingprice)
 
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+#Get the day before yesterday's closing stock price
+dayb_yesterday = data_list[1]
+dayb_yesterday_closingprice = float(dayb_yesterday["4. close"])
+print(dayb_yesterday_closingprice)
 
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+#Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
+# Hint: https://www.w3schools.com/python/ref_func_abs.asp
+difference = abs(yesterday_closingprice - dayb_yesterday_closingprice)
+print(difference)
+
+#TODO 4. - Work out the percentage difference in price between closing price yesterday and
+# closing price the day before yesterday.
+
+
 
 #TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
 
