869e573dc6a14605d407f03298cc60e3663e77b3
changeAJay <ajajay008@gmail.com>
2022-03-25 20:01:14 +0530

    Sends news notification

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
+from twilio.rest import Client
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+TWILIO_SID = "AC1498874af0097554fa2a0e957aaa1e76"
+TWILIO_AUTH_TKN = "0ca0810d7ac2c40e23d1fe5d2fbcf1ab"
 
 parameters = {
     "function": "TIME_SERIES_DAILY",
@@ -13,39 +16,24 @@ parameters = {
     "apikey": "P0HM4GKCWWF3ECIG"
 }
 
-# STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
 news_response = requests.get(STOCK_ENDPOINT, params=parameters)
 data = news_response.json()["Time Series (Daily)"]
 data_list = [value for (key, value) in data.items()]
 
-#Get yesterday's closing stock price. Hint: Yo can perform list comprehensions on Python dictionaries.
-# e.g. [new_value for (key, value) in dictionary.items()]
+
 yesterday_data = data_list[0]
 yesterday_closingprice = float(yesterday_data["4. close"])
 
-
-#Get the day before yesterday's closing stock price
 dayb_yesterday = data_list[1]
 dayb_yesterday_closingprice = float(dayb_yesterday["4. close"])
 
 
-#Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
-# Hint: https://www.w3schools.com/python/ref_func_abs.asp
 difference = abs(yesterday_closingprice - dayb_yesterday_closingprice)
-print(difference)
 
-#Work out the percentage difference in price between closing price yesterday and
-# closing price the day before yesterday.
 percent = (difference / float(yesterday_closingprice)) * 100
 
 print(percent)
 
-#If TODO4 percentage is greater than 5 then print("Get News").
-# Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-# STEP 2: https://newsapi.org/
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 if percent > 1:
     para = {
         "apikey": "cc94947cd3504922825f93714cf76843",
@@ -54,30 +42,21 @@ if percent > 1:
 
     news_response = requests.get(url=NEWS_ENDPOINT, params=para)
     articles = news_response.json()["articles"]
-    print(articles)
+    three_articles = articles[:3]
+    #print(three_articles)
+
+    formatted_articles = [f"Headline: {item['title']}\n Brief: {item['description']}"
+                          for item in three_articles]
+
+    client = Client(TWILIO_SID, TWILIO_AUTH_TKN)
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_="+15734051285",
+            to="VerifiedNumber"
+        )
+        print(message.status)
 
 
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles.
-#Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
 
-
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
-
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
-
-
-
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
